02839e6c8a6cf14c77519981f21d03468d019a5b
e-navi <e-navi@murotec.com>
2026-09-15 11:56:53 +0900

    高DPI環境でのレイアウト崩れを防ぐためSetHighDpiMode(PerMonitorV2)を削除

--- Site7DbEditor/Program.cs
+++ Site7DbEditor/Program.cs
@@ -35,7 +35,6 @@ namespace Site7DbEditor
 
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             ApplicationConfiguration.Initialize();
-            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
 
             // コマンドライン引数で直接DBが渡された場合は直接エディタを起動
             if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
